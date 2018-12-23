b50bf8ca363b660b5b76d6aaea11d5e844f5a6db
GreengoDK <dmitry.kolos@gmail.com>
2018-12-23 12:15:36 +0500

    Added newConsoleApp in solution

---
+++ US4/CodeConverter_Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Windows.Forms;
+
+namespace CodeConverter_Console
+{
+    class Program
+    {
+        public struct associationsStruct
+        {
+            string includeLibrary;
+            string type;
+            List<string> _US_Names;
+            string _CPP_Association;
+            string isChildOf;
+        };
+        public StreamReader inputDocument;
+        public StreamWriter outputDocumentH;
+        public StreamWriter outputDocumentCpp;
+        public OpenFileDialog openFileDialog;
+        public List<string> associations;
+
+        static void Main(string[] args)
+        {
+
+            OpenFileDialog();
+            Console.ReadKey();
+        }
+        private void OpenFile()
+        {
+
+        }
+    }
+}
